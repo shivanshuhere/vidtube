7c9eb6842dfabb2330cb39543c4b6ae4ab46c9e4
Shivanshu Koundal <126767653+shivanshuhere@users.noreply.github.com>
2024-10-19 18:59:10 +0530

    add expresss app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("duck you from, backend!");
+import { app } from "./app.js";
+import { connectDB } from "./db/index.js";
+const PORT = process.env.PORT || 8080;
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is listening at port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("mongoDB connection error ::", err);
+  });
