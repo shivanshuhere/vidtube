04e39a5bfb8247ffba96fb06d0c4701358e76d77
Shivanshu Koundal <126767653+shivanshuhere@users.noreply.github.com>
2024-10-22 16:12:09 +0530

    add : plugin, tokens and password encryption

--- src/models/users.models.js
+++ src/models/users.models.js
@@ -1,4 +1,7 @@
 import mongoose, { Schema } from "mongoose";
+import aggregatePaginate from "mongoose-aggregate-paginate-v2";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
 
 const userSchema = Schema(
   {
@@ -42,10 +45,47 @@ const userSchema = Schema(
     refreshToken: {
       type: String,
     },
+    accessToken: {
+      type: String,
+    },
   },
   {
     timestamp: true,
   }
 );
 
+userSchema.pre("save", async function (next) {
+  if (this.modified("password")) return next();
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.mehtods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.mehtods.generateAccessToken = function () {
+  jwt.sign(
+    {
+      _id: this._id,
+      username: this.username,
+      email: this.email,
+      fullname: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: "10d" }
+  );
+};
+
+userSchema.mehtods.generateRefreshToken = function () {
+  jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: "1d" }
+  );
+};
+
+userSchema.plugin(aggregatePaginate);
 export const User = mongoose.model("User", userSchema);
