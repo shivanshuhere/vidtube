de2d8b20ae0ff54371521c4a8d0c2a505dcffc84
Shivanshu Koundal <126767653+shivanshuhere@users.noreply.github.com>
2024-10-28 15:37:08 +0530

    chnage: folder path

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -2,11 +2,13 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "/public/temp");
+    cb(null, "public/temp");
   },
+  // filename: function (req, file, cb) {
+  //   // const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+  //   cb(null, file.originalname);
   filename: function (req, file, cb) {
-    // const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cd(null, file.originalname);
+    cb(null, file.originalname);
   },
 });
 
